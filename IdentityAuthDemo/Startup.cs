6a4b8f78d60fc81025a5fcaba4f20a835584990a
aha_q <dandelion2110@gmail.com>
2025-04-26 17:06:44 +1000

    Create scalffolding for identity and authentication project

---
+++ IdentityAuthDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityAuthDemo.Startup))]
+namespace IdentityAuthDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
